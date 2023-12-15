5db6e5964ff50fe37c52593db92a5ba46b97f94a
the-woka <ois.woka23@gmail.com>
2023-12-15 20:00:15 +0700

    Update admin page with new features and fix drag and drop bugs. (generated by blackbox.ai )

--- js/index.js
+++ js/index.js
@@ -85,3 +85,17 @@ pencarian.addEventListener("mouseup", dragStop);
 cerita.addEventListener("mouseup", dragStop);
 
 // drag class ends
+
+// Admin Page
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
