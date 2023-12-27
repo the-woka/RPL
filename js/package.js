99389712c3ac5f0476c83301df10c7bc101cb8c1
Adrian Yoris Mbake Woka <122039909+the-woka@users.noreply.github.com>
2023-12-27 18:04:41 +0700

    Add files via upload

--- js/package.js
+++ js/package.js
@@ -3,15 +3,47 @@ const tabList = tabbedDay.querySelector(".ticket");
 const tabButtons = tabList.querySelectorAll(".ticketDay");
 const tabPanels = tabbedDay.querySelectorAll(".infoPaket > div");
 
+tabList.setAttribute("role", "tablist");
+
+tabList.querySelectorAll('li').forEach((listitem) => {
+  listitem.setAttribute("role", "presentation");
+})
+
 tabButtons.forEach((tab, index) => {
+  tab.setAttribute("role", "tab");
   if (index === 0) {
+    tab.setAttribute("aria-selected", "true");
   } else {
+    tab.setAttribute("tabindex", "-1");
     tabPanels[index].setAttribute("hidden", "");
   }
 });
 
-tabbedDay.addEventListener("click", (e) => {
-  const clickedTab = e.target.closest("a");
-  console.log(e.target);
-  e.preventDefault();
+tabPanels.forEach((panel) => {
+  panel.setAttribute("role", "tabpanel");
+  panel.setAttribute("tabindex", "0");
 });
+
+tabbedDay.addEventListener("click", (e) => {
+  const clickedTab = e.target.closest(".ticketDay");
+  if(!clickedTab) return;
+  e.preventDefault();
+
+  switchTab(clickedTab);
+});
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute('href');
+  const activePanel = tabbedDay.querySelector(activePanelId);
+
+  tabButtons.forEach((button) => {
+    button.setAttribute("aria-selected", false);
+    button.setAttribute("tabindex", "-1");
+  });
+
+  tabPanels.forEach((panel) => {
+    panel.setAttribute("hidden", true);
+  });
+  activePanel.removeAttribute("hidden", false);
+  newTab.setAttribute("aria-selected", true);
+}
