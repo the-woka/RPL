3b5fc61067457e693f284acc817a2d2d7855d569
Adrian Yoris Mbake Woka <122039909+the-woka@users.noreply.github.com>
2023-12-27 21:59:41 +0700

    Add files via upload
    Menyempurnakan package.js

--- js/package.js
+++ js/package.js
@@ -2,9 +2,6 @@ const tabbedDay = document.querySelector(".tabbed-day");
 const tabList = tabbedDay.querySelector(".ticket");
 const tabButtons = tabList.querySelectorAll(".ticketDay");
 const tabPanels = tabbedDay.querySelectorAll(".infoPaket > div");
-const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
 
 tabList.setAttribute("role", "tablist");
 
@@ -51,10 +48,31 @@ function switchTab(newTab) {
   newTab.setAttribute("aria-selected", true);
 }
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
+// Menangani setiap tombol "Beli"
+// Menggabungkan kedua potongan kode
+const closeButtons = document.querySelectorAll('.close-button');
+const buyButtons = document.querySelectorAll('.open-button');
+
+// Menangani setiap tombol "Beli"
+buyButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const modalId = `modal${index + 1}`; // Membuat ID modal yang sesuai
+    const modal = document.getElementById(modalId);
+    
+    if (modal) {
+      modal.showModal(); // Tampilkan modal yang sesuai
+    }
+  });
 });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
+// Menangani setiap tombol "Batal" atau "Close"
+closeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal'); // Mendapatkan modal terdekat dari tombol yang ditekan
+    
+    if (modal) {
+      modal.close(); // Menutup modal
+    }
+  });
 });
+
